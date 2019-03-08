1eefc49edc71a0960564600caa6f8a7b64ce11e6
Javier <47282213+javiciier@users.noreply.github.com>
2019-03-08 14:41:44 +0100

    Finished "iteracion 1"
    Added coments and JavaDoc in all methods.
    Uploaded new test file that allows checking correct performance of web server for basic operations (GET, HEAD).
    Uploaded directory containing most basic resources server can handle.
    ~ServidorHTTP~
    * Modified local path to server into a local path on my own computer.
    * Added main() method to allow class being executable.
    ~PeticionHTTP~
    * Added console text in handled exceptions to simplify debugging.
    * Modified local path to resources in methods HEADhttp() and GEThttp().
    * Added new boolean parameter to method mostrarRespuesta() to indicate when to show the body of a request like GET. Also modified each appearence of this method in whole class.
    * Reestructured code in mostrarRespuesta() to adapt its format into server test file.
    * Implemented method mostrarCuerpoGET() that writes in console the body content of a resource (<body></body> content in HTML file)
    * Now getTipoContenido() is able to distinguish most basic files extensions, returning its value (not its . extension)
    * Implemented override Thread method run() to make server executable.

--- PeticionHTTP.java
+++ PeticionHTTP.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Date;
 
+
 /**
 * Clase encargada de realizar las peticiones HTTP al servidor web.
 * Soporta ejecución multihilo para poder realizar varias peticiones simultaneas.
@@ -15,11 +16,11 @@ import java.util.Date;
 public class PeticionHTTP extends Thread {
     // ATRIBUTOS
     private final Socket cliente;                                               // Cliente que realiza la petición
-    private BufferedReader entrada = null;                                      // Información a leer en la entrada
-    private OutputStream salida = null;                                         // Datos de salida
+    private BufferedReader entrada;                                      // Información a leer en la entrada
+    private OutputStream salida;                                         // Datos de salida
     private final CabeceraHTTP cabecera;
-    private String lineaComandos = "";                                          // Comando que se solicita realizar
-    public enum EstadoHTTP {							// Posibles estados a recibir cuando se realiza una solicitud HTTP
+    private String lineaComandos;                                          // Comando que se solicita realizar
+    enum EstadoHTTP {							// Posibles estados a recibir cuando se realiza una solicitud HTTP
     	OK("200 OK"),								// Recurso se recibió sin errores
     	NOT_FOUND("404 Not Found"),						// Recurso no se encontró o no existe
         BAD_REQUEST("400 Bad Request");
@@ -27,18 +28,16 @@ public class PeticionHTTP extends Thread {
 
     	private final String estado;
 
-    	// CONSTRUCTOR DEL ENUMERADO
+    	// CONSTRUCTORES
     	EstadoHTTP(String estado) {
     		this.estado = estado;
     	}
 
-    	// GETTER DEL ENUMERADO
+    	// GETTERS
     	public String getEstadoHTTP() {
     		return this.estado;
     	}
     }
-
-    public File fichero;                                                        // Fichero .html que se solicita al servidor
     
     // CONSTRUCTORES
     /**
@@ -64,13 +63,13 @@ public class PeticionHTTP extends Thread {
      * Formato: <peticion> <recurso> <versionHTTP>
      */
     private void HEADhttp() {
-    	String ruta = this.lineaComandos.split(" ")[1];				// Obtiene el recurso solicitado
-    	File recurso = new File(ServidorHTTP.rutaServidor + ruta);		// Busca el recurso en el mismo directorio o subdirectorios en donde está ubicado el servidor
+    	String[] ruta = this.lineaComandos.split(" ");				// Obtiene el recurso solicitado
+    	File recurso = new File("/home/fic/Escritorio/Redes/Practicas/p1/ficheros" + ruta[1]);		// Busca el recurso en el mismo directorio o subdirectorios en donde está ubicado el servidor
     	
-    	if (!recurso.exists())							// Si el recurso buscado existe en algún directorio
-            mostrarRespuesta(EstadoHTTP.NOT_FOUND, null);			// Muestra ERROR 404 porque no existe archivo. Al no existir, no le pasa como argumento ningún recurso
+    	if ( recurso.exists() )                                                // Si el recurso buscado existe en algún directorio
+            mostrarRespuesta(EstadoHTTP.OK, recurso, false);                           // Petición correcta. Muestra la información del recurso recibido como argumento
     	else
-            mostrarRespuesta(EstadoHTTP.OK, recurso);                           // Petición correcta. Muestra la información del recurso recibido como argumento
+            mostrarRespuesta(EstadoHTTP.NOT_FOUND, null, false);			// Muestra ERROR 404 porque no existe archivo. Al no existir, no le pasa como argumento ningún recurso
     }
 
     /**
@@ -78,80 +77,104 @@ public class PeticionHTTP extends Thread {
      */
     private void GEThttp() {
         String[] comandos = this.lineaComandos.split(" ");                      // Separa los elementos de la línea de comandos. Formato <COMANDO> <RUTA> <VERSION_HTTP>
-        File recurso = new File(ServidorHTTP.rutaServidor + comandos[1]);       // Obtiene el recurso buscado a partir de la ruta del servidor y la ruta especificada.
-        Date fechaModifServidor, fechaModifCliente;
-        String index;                                                           // Ruta hacia el recurso principal. Por defecto es el fichero index.html
+        File recurso = new File("/home/fic/Escritorio/Redes/Practicas/p1/ficheros" + comandos[1]);       // Obtiene el recurso buscado a partir de la ruta del servidor y la ruta especificada.
+        //PrintWriter canalSalida = new PrintWriter(this.salida, true);         // Permite mostrar información por pantalla
         
-        if (!recurso.exists()) {                                                // Si el recurso no existe
-            mostrarRespuesta(EstadoHTTP.NOT_FOUND, null);
-        }
-        else {                                                                  // El recurso SÍ existe
-            try {
-                if ( !recurso.isDirectory() ) {                                   // El recurso no es un directorio
-                    fechaModifCliente = this.cabecera.getFecha();               // Obtiene la fecha de modificación del recurso solicitado
-                    fechaModifServidor = new Date(recurso.lastModified());      // Obtiene la fecha de modificación del recurso obtenido por el servidor
-                    if (fechaModifServidor.compareTo(fechaModifCliente) > 0 )   // Comprueba si la fecha de modificación del recurso en el servidor es posterior a la fecha del recurso
-                        mostrarRespuesta(EstadoHTTP.OK, recurso);
-                    else                                                        // Recurso ha sido modificado después del almacenado en el servidor, por lo tanto no puede encontrar el recurso exacto
-                        mostrarRespuesta(EstadoHTTP.NOT_FOUND, null);
-                }  // fin if
-                else {                                                          // Recurso recibido ES un directorio
-                    if ( !comandos[1].endsWith("/") )                           // Si el directorio no acaba con "/", se le añade
-                        comandos[1] += "/";
-                    index = ( ServidorHTTP.rutaServidor + comandos[1] + ServidorHTTP.recursoPorDefecto);  // Obtiene el nombre del recurso principal que entrega el servidor
-                    File recursoPrincipal = new File(index);                    // Carga el recurso principal
-                    if (recursoPrincipal.exists())                              // Si recurso principal existe, lo muestra
-                        mostrarRespuesta(EstadoHTTP.OK, recursoPrincipal);
-                }  // fin else
-            }
-            catch (NullPointerException NPex) {
-                System.err.println("Error: " + NPex.getMessage());
-            }
+        if ( recurso.exists() ) {						// Si el recurso EXISTE, lo muestra
+            mostrarRespuesta(EstadoHTTP.OK, recurso, true);
+        } // fin if
+        else {									// Recurso no existe -> ERROR 404											// El recurso no existe
+            mostrarRespuesta(EstadoHTTP.NOT_FOUND, null, true);
         }  // fin else
     }
-    
     
     /**
      * Método encargado de mostrar por pantalla la información asociada a la petición realizada.
      * @param estado 	Estado que debería devolver la solicitud según su situación (archivo encontrado, nombre incorrecto, etc)
      * @param recurso 	Fichero del cual obtiene la información solicitada
+     * @param mostrarCuerpo     Booleano que indica si el método debe devolver el contenido del fichero
      */
-    private void mostrarRespuesta(EstadoHTTP estado, File recurso) {
+    private void mostrarRespuesta(EstadoHTTP estado, File recurso, boolean mostrarCuerpo) {
     	PrintWriter canalSalida = new PrintWriter(this.salida, true);		// Establece un canal de salida para mostrar información por pantalla
     	String textoEstado;							// Texto que se mostrará según el estado de la petición
         Date fechaRespuesta;                                                    // Almacena la fecha en la que se generó la petición
-        Date ultimaModificacion;
     
+        // LÍNEA DE ESTADO
     	switch (estado) {							// Muestra distinta información por el canal de salida según el estado de la petición recibida
             case OK:
-                // LÍNEA DE ESTADO
-                textoEstado = ( ServidorHTTP.versionServidor + " " + estado.OK.getEstadoHTTP() );
+                textoEstado = ( this.lineaComandos.split(" ")[2] + " " + estado.getEstadoHTTP() );
                 canalSalida.println(textoEstado);
-                // LÍNEAS DE CABECERA
-                fechaRespuesta = new Date();
-                canalSalida.println("Date: " + fechaRespuesta);
-                canalSalida.println("Server: " + ServidorHTTP.nombreServidor);
-                if ( recurso != null ) {                                        // Si recibe un recurso muestra su información
-                    ultimaModificacion = new Date(recurso.lastModified());
-                    canalSalida.println("Last-Modified: " + ultimaModificacion);
-                    canalSalida.println("Content-Length: " + recurso.length());
-                    canalSalida.println("Content-Type: " + getTipoContenido(recurso.getName()));
-                }
-            break;  // fin OK
+            break;
             case NOT_FOUND:
-                // LÍNEA DE ESTADO
-                textoEstado = ( ServidorHTTP.versionServidor + " " + estado.NOT_FOUND.getEstadoHTTP() );
+                textoEstado = ( this.lineaComandos.split(" ")[2] + " " + estado.getEstadoHTTP() );
                 canalSalida.println(textoEstado);
-                // LÍNEAS DE CABECERA
-                
+                //recurso = new File("/home/fic/Escritorio/Redes/Practicas/p1/ficheros/notFound.html");
             break;  // fin NOT_FOUND
             case BAD_REQUEST:
-            	// LÍNEA DE ESTADO
-            	textoEstado = ( ServidorHTTP.versionServidor + " " + estado.BAD_REQUEST.getEstadoHTTP() );
-            	// LÍNEAS DE CABECERA
-            	
+            	textoEstado = ( this.lineaComandos.split(" ")[2] + " " + estado.getEstadoHTTP() );
+                canalSalida.println(textoEstado);
+                //recurso = new File("/home/fic/Escritorio/Redes/Practicas/p1/ficheros/BAD_REQUEST.html");
+                /* canalSalida.println();
+                mostrarCuerpoGET(recurso);                                      // Muestra recurso BAD REQUEST */
             break;
-    	}// fin switch
+            default:
+                canalSalida.println("Error: respuesta no válida");
+    	} // fin switch
+        
+        // LÍNEAS DE CABECERA
+        fechaRespuesta = new Date();
+        canalSalida.println("Date: " + fechaRespuesta);
+        canalSalida.println("Server: " + ServidorHTTP.nombreServidor);
+        if ( recurso != null ) {                                        // Si recibe un recurso muestra su información
+            Date ultimaModificacion = new Date(recurso.lastModified());
+            canalSalida.println("Last-Modified: " + ultimaModificacion);
+            canalSalida.println("Content-Length: " + recurso.length());
+            canalSalida.println("Content-Type: " + getTipoContenido(recurso.getName()));
+            canalSalida.println();
+        }
+        
+        // CUERPO DE LA PETICIÓN
+        if ( mostrarCuerpo && (recurso != null) ) {
+            mostrarCuerpoGET(recurso);
+        }
+    }
+    
+    /**
+    * Método que muestra por pantalla el contenido de un fichero.
+    * Este método se emplea en GEThttp() para mostrar el contenido de un fichero HTML.
+    * @param recurso    Fichero a partir del cual se obtiene el contenido a devolver.
+    */
+    private void mostrarCuerpoGET(File recurso) {
+        FileInputStream origen = null;                                          // Fuente de donde se obtienen los datos
+        byte[] contenido = new byte[1024];                                      // Array en donde se guarda el contenido leido (byte porque puede ser de cualquier tipo)
+        int tamanho;
+        
+        try {
+            origen = new FileInputStream(recurso);                              // Abre el fichero origen (si no lo encuentra genera FileNotFoundException)
+            while ( (tamanho = origen.read(contenido)) != -1 ) {
+                this.salida.write(contenido, 0, tamanho);
+            }
+            //System.out.println();
+        }
+        catch (FileNotFoundException FNFexc) {
+            System.err.println("Error: no se encuentra fichero " +
+                    this.lineaComandos.split(" ")[1].substring(1));
+            //System.err.println("Ruta a fichero recibida: " + recurso.getPath());
+        }
+        catch (IOException IOexc) {
+            System.err.println("Error: " + IOexc.getMessage());
+        }
+        finally {
+            if (origen != null) {
+                try {
+                    origen.close();
+                }
+                catch (IOException IOexc) {
+                    System.err.println("Error: no se pueden liberar recursos.");
+                }
+            } // fin if
+        }
+        
     }
     
     /**
@@ -161,7 +184,7 @@ public class PeticionHTTP extends Thread {
      */
     private String getTipoContenido(String fichero) {
         int puntoExtension = fichero.lastIndexOf(".");                          // Calcula la posición, en el nombre del recurso, en donde empieza el . que indica la extensión del fichero
-        String extension = fichero.substring(puntoExtension);                   // Devuelve la posición en donde empieza la extensión de fichero (.html, .txt, .gif, .png)
+        String extension = fichero.substring(puntoExtension + 1);                // Devuelve la posición en donde empieza la extensión de fichero, sin el punto (html, txt, .gif, png)
         String tipo;                                                            // Tipo de fichero. (Variable necesaria porque return impide ejecutar break dentro de switch)
         
         switch (extension) {
@@ -195,7 +218,7 @@ public class PeticionHTTP extends Thread {
         String peticion;
         try {
             this.lineaComandos = this.entrada.readLine();                       // Obtiene la petición realizada y la almacena (posible IOException)
-            if (this.lineaComandos != null) {                                   // Si recibió la petición correctamente
+            if ( this.lineaComandos != null ) {                                   // Si recibió la petición correctamente
                 System.out.println(this.lineaComandos);                         // Muestra la petición solicitada por pantalla
                 peticion = this.lineaComandos.split(" ")[0];                    // Obtiene qué petición realizar (GET, HEAD, etc)
                 switch (peticion) {
@@ -206,12 +229,15 @@ public class PeticionHTTP extends Thread {
                         GEThttp();
                     break;
                     default:                                                    // Recibe una petición que no se comprende (mal escrita, no existe, etc)
-                        mostrarRespuesta(EstadoHTTP.BAD_REQUEST, null);
+                        mostrarRespuesta(EstadoHTTP.BAD_REQUEST, null, false);
                 } // fin switch
             } // fin if
         } // fin try
-        catch (IOException IOex) {
-            System.err.println("Error: " + IOex.getLocalizedMessage());
+        catch (SocketException Sexc) {
+            System.err.println("Socket error: " + Sexc.getMessage());
+        }
+        catch (IOException IOexc) {
+            System.err.println("Runtime error: " + IOexc.getMessage());
         }
         finally {                                                               // Cerramos los canales de entrada y salida y capturamos sus posibles excepciones
             try {
@@ -219,8 +245,9 @@ public class PeticionHTTP extends Thread {
                 this.salida.close();
                 this.cliente.close();
             }
-            catch (IOException IOex) {
-                throw new RuntimeException(IOex);
+            catch (IOException IOexc) {
+                System.err.println("Error: no se pueden liberar recursos.");
+                throw new RuntimeException(IOexc);
             }
         } // fin finalñy
     }
--- ServidorHTTP.java
+++ ServidorHTTP.java
@@ -9,8 +9,9 @@ import java.net.*;
 
 /**
 * Servidor multithread encargado de gestionar peticiones web HTTP.
-* Así mismo, se encarga de crear registros de su actividad.
-* El servidor también puede configurarse a partir de un fichero de configuración.
+* A partir de la iteracion2 podrá realizar tareas como:
+* * Crear ficheros con registros de su actividad (peticiones recibidas, errores)-
+* * Autoconfiguración a partir de un fichero de configuración.
 */
 public class ServidorHTTP {
     // ATRIBUTOS
@@ -18,12 +19,11 @@ public class ServidorHTTP {
     private Socket cliente;
     private int puerto = 5000;                                                  // Puerto que ocupará el servidor
     private final int tiempoEspera = 60;                                        // Tiempo que espera por peticiones (en segundos)
-    public static int puertoServidor;                                           // Puerto para comunicarse con el servidor web
     private PeticionHTTP peticionHTTP;                                          // Gestiona las peticiones HTTP que recibe el servidor
-    public static String rutaServidor;						// Indica el directorio donde se ubica el servidor dentro del ordenador
-    public static String versionServidor = "HTTP 1.0";                          // Versión de HTTP del servidor. Iteracion 1: HTTP 1.0; Iteracion 2: HTTP 1.1
-    public static String nombreServidor;                                        // Nombre asignado al servidor
-    public static String recursoPorDefecto = "index.html";                      // Recurso que el servidor entregará por defecto cuando reciba una petición
+    public static String rutaServidor = "/home/fic/Escritorio/Redes/Practicas/p1";         // Indica el directorio donde se ubica el servidor dentro del ordenador
+    public static String versionServidor = "HTTP/1.0";                          // Versión de HTTP del servidor. Iteracion 1: HTTP 1.0; Iteracion 2: HTTP 1.1
+    public static String nombreServidor = "ServidorHTTP-iteracion1";            // Nombre asignado al servidor
+    //public static String recursoPorDefecto = "index.html";                      // Recurso que el servidor entregará por defecto cuando reciba una petición
     
     // CONSTRUCTORES
     /**
@@ -31,14 +31,14 @@ public class ServidorHTTP {
     */
     public ServidorHTTP() {
         try {
-            this.servidor = new ServerSocket(this.puerto);                      // Crea el servidor en el puerto indicado
-            System.out.println("Servidor abierto en puerto " + this.puertoServidor);
-            System.out.println("Servidor creado en puerto " + puerto);
+            this.servidor = new ServerSocket(this.puerto);                      // Crea el servidor en el puerto indicado (genera IOException si puerto está ocupado)
+            System.out.println("Servidor abierto en puerto " + this.puerto);
             this.servidor.setSoTimeout(tiempoEspera*1000);                      // Establece tiempo de espera indicado (x1000 ms)
             System.out.println("Tiempo de espera establecido: " + this.tiempoEspera + " segundos.");
         }
         catch (IOException IOexc) {
-            System.out.println("Error: " + IOexc.getMessage());
+            System.err.println("Runtime Error: puerto " + this.puerto + " está ocupado.");
+            System.err.println("Error: " + IOexc.getMessage());
         }
     }
     
@@ -47,26 +47,34 @@ public class ServidorHTTP {
         try {
             while (true) {          // Bucle infinito para que servidor siempre escuche peticiones
                 this.cliente = this.servidor.accept();                          // Servidor acepta conexión al recibirla (puede generar IOException)
+                //System.out.println("Conexión aceptada.");
                 this.peticionHTTP = new PeticionHTTP(this.cliente);             // Crea una nueva petición HTTP que gestiona la conexión recibida
-                System.out.println("Conexión aceptada.");
                 this.peticionHTTP.start();                                      // Comienza a ejecutar el nuevo thread creado anteriormente
             }
         }
         catch (SocketTimeoutException TOexc) {
-            System.err.println("Tiempo agotado." + tiempoEspera + "segundos sin recibir peticiones");
+            System.err.println("Tiempo agotado. " + tiempoEspera + " segundos sin recibir peticiones");
         }
         catch (IOException IOexc) {
-            System.out.println("Error: " + IOexc.getMessage());
+            System.out.println("Error E/S: " + IOexc.getMessage());
         }
         finally {                                                               // Si no recibe ninguna petición, cierra el puerto y captura posibles excepciones
             try {
                 this.servidor.close();
             }
             catch (IOException IOexc) {
-                System.err.println("Error: " + IOexc.getMessage());
+                System.err.println("Error: no se pueden liberar recursos.");
+                throw new RuntimeException(IOexc);
             }
         }
     }
-
-
+    
+    // MÉTODO MAIN
+    /**
+    * Se encarga de crear una instancia del servidor para que escuche las peticiones recibidas. 
+    */
+    public static void main(String[] args) {
+        ServidorHTTP servidor = new ServidorHTTP();                             // Crea una instancia del servidor
+        servidor.escucharPeticiones();                                          // Servidor comienza a ejecutarse
+    }
 }
